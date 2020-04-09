bd7988f015a3c28bb905e19c65400ea2c3eae625
kadmosb1 <k.j.vanderlelij@hhs.nl>
2020-04-09 18:20:40 +0200

    Code toegevoegd om replace-temp-with-query te kunnen demonstreren.

--- src/Factuur.java
+++ src/Factuur.java
@@ -10,24 +10,29 @@ public class Factuur {
 
         double totaalprijs = 0.0;
 
-        regel1 = new FactuurRegel("Product 1", 2.50, 20);
+        regel1 = new FactuurRegel("Product 1", 2.50);
+        regel1.setAantalProducten(20);
         totaalprijs += regel1.getTotaalprijs();
-        regel2 = new FactuurRegel("Product 2", 10.00, 1);
+        regel2 = new FactuurRegel("Product 2", 10.00);
+        regel2.setAantalProducten(1);
         totaalprijs += regel2.getTotaalprijs();
-        regel3 = new FactuurRegel("Product 3", 0.22, 1000);
+        regel3 = new FactuurRegel("Product 3", 0.22);
+        regel3.setAantalProducten(1000);
         totaalprijs += regel3.getTotaalprijs();
-        regel4 = new FactuurRegel("Product 4", 0.88, 10);
+        regel4 = new FactuurRegel("Product 4", 0.88);
+        regel4.setAantalProducten(100);
         totaalprijs += regel4.getTotaalprijs();
 
         klant = new Klant ("Haagse Hogeschool", "Johanna Westerdijkplein", 75, "", 2521, "EN", "DEN HAAG");
 
-        System.out.println (klant);
-        System.out.println ("\n\n");
+        System.out.println (klant + "\n\n");
+        // "%6d %-30s  €%8.2f     %3.0f%%  €%8.2f"
+        System.out.format ("aantal %-30s   %8s  %7s   %8s%n", "Naam product", "prijs/st", "korting", "totaal");
         System.out.println (regel1);
         System.out.println (regel2);
         System.out.println (regel3);
         System.out.println (regel4);
-        System.out.format ("     %40s            ________%n", "");
-        System.out.format ("     %40s           %9.2f%n%n", "Totaal te betalen:", totaalprijs);
+        System.out.format ("       %-30s   %8s  %7s  %9s%n", "", "", "", "_________");
+        System.out.format ("       %-30s   %8s  %7s  €%8.2f%n%n", "Totaal te betalen:", "", "", totaalprijs);
     }
 }
--- src/FactuurRegel.java
+++ src/FactuurRegel.java
@@ -1,18 +1,53 @@
 public class FactuurRegel {
     private Product product;
-    private int aantal;
+    private int aantalProducten;
+    private double kortingspercentage;
 
-    public FactuurRegel (String productnaam, double productprijs, int aantal) {
+    public FactuurRegel (String productnaam, double productprijs) {
         product = new Product (productnaam, productprijs);
-        this.aantal = aantal;
+        kortingspercentage = 0.0;
+    }
+
+    public void setAantalProducten (int aantal) {
+        aantalProducten = aantal;
     }
 
     public String toString () {
-        double totaalprijs = aantal * product.getPrijs();
-        return String.format("%4d %40s  %8.2f  %8.2f", aantal, product.getNaam(), product.getPrijs (), totaalprijs);
+
+        double prijsMetKorting = aantalProducten * product.getPrijs ();
+
+        /*
+         * Vanaf 100 stuks van een product geldt een kortingspercentage van 2%.
+         * Vanaf 1.000 stuks geldt een kortingspercentage van 3%.
+         */
+        if (aantalProducten >= 1000) {
+            kortingspercentage = 3.0;
+        }
+        else if (aantalProducten >= 100) {
+            kortingspercentage = 2.0;
+        }
+
+        prijsMetKorting *= (100.0 - kortingspercentage) / 100.0;
+        return String.format("%6d %-30s  €%8.2f     %3.0f%%  €%8.2f", aantalProducten, product.getNaam(), product.getPrijs (), kortingspercentage, prijsMetKorting);
     }
 
     public double getTotaalprijs () {
-        return aantal * product.getPrijs ();
+
+        double prijsMetKorting = aantalProducten * product.getPrijs ();
+
+        /*
+         * Vanaf 100 stuks van een product geldt een kortingspercentage van 2%.
+         * Vanaf 1.000 stuks geldt een kortingspercentage van 3%.
+         */
+        if (aantalProducten >= 1000) {
+            kortingspercentage = 3.0;
+        }
+        else if (aantalProducten >= 100) {
+            kortingspercentage = 2.0;
+        }
+
+        prijsMetKorting *= (100.0 - kortingspercentage) / 100.0;
+
+        return prijsMetKorting;
     }
 }
